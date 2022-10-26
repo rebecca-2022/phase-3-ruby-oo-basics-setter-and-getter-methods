ae4e9612221d2ea0f9275b249f0de8c700842617
rebecca njeri <rebecca.njeri@student.moringaschool.com>
2022-10-26 21:18:54 +0300

    completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+    #setter method
+    def name= (name)
+        @name = name
+    end
+    def breed= (breed)
+        @breed = breed
+    end
 
-end
+    #getter method
+    def name
+        @name
+    end
+    def breed
+        @breed
+    end
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,25 @@
+require 'pry'
 class Person
 
+    #setter method
+    def name= (name)
+        @name = name
+    end
+    def job= (job)
+        @job = job
+    end
+
+    #getter method
+    def name
+        @name
+    end
+    def job
+        @job
+    end
+
 end
+kanye = Person.new
+kanye.name= "Grace"
+kanye.job = "Software Engineer"
+binding.pry
+0
